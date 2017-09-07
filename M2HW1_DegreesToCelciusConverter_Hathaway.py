51dae4763aa690b5d3ad8d987de4d74d128ddcea
Hathawam3812 <31741725+Hathawam3812@users.noreply.github.com>
2017-09-07 16:15:11 -0400

    Add files via upload
    This converter I made on my own

---
+++ M2HW1_DegreesToCelciusConverter_Hathaway.py
@@ -0,0 +1,18 @@
+# COURSE NUMBER
+print("CTI-110")
+# ASSIGNMENT - LESSON NAME
+print("Degrees To Celcius Converter")
+# LAST NAME
+print("Hathaway")
+# DATE
+print("07SEP2017")
+print(" ")
+print(" ")
+print(" ")
+
+# Degrees to Celcius Coverter
+print("Degrees to Celcius Converter")
+Temp = float(input("What is the Temp in degrees? "))
+# Multiplies Degrees by 1.5 then adds 32
+Celcius = Temp * 1.8 + 32
+print("The Temperature in Celcius is:", Celcius)
