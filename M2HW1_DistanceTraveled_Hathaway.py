1a22e239a7a0511fb0f632bb4b1cba85ed44cba3
Hathawam3812 <31741725+Hathawam3812@users.noreply.github.com>
2017-09-07 20:07:05 -0400

    Add files via upload
    First attempt.

---
+++ M2HW1_DistanceTraveled_Hathaway.py
@@ -0,0 +1,31 @@
+# COURSE NUMBER
+print("CTI-110")
+# ASSIGNMENT - LESSON NAME
+print("M2HW1 - DistanceTraveled")
+# LAST NAME
+print("Hathaway")
+# DATE
+print("07SEP2017")
+print(" ")
+print(" ")
+print(" ")
+
+# Rate has been replaced with Speed to make more sense
+# Rate could be mistaken for a dollar amount per mile
+print("Input Speed and Distance")
+# Distance is defined in miles.
+print(" ")
+distance = float(input("distance in miles: "))
+# Speed is defined in miles per hour.
+print(" ")
+speed = float(input("speed in MPH: "))
+# Time is defined as distance divided by speed.
+print(" ")
+time = distance / speed
+# Output is the time elapsed to travel the distance at the given speed.
+print("time: ", distance / speed)
+
+
+
+
+
