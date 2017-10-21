8c60619e73fb4dcd129eb043a190b707b82e2df3
Hathawam3812 <31741725+Hathawam3812@users.noreply.github.com>
2017-10-21 17:23:42 -0400

    Add files via upload

---
+++ M5HW1_DistanceTraveled_Hathaway.py
@@ -0,0 +1,22 @@
+# CTI-100
+# M5HW1_DistanceTraveled_Hathaway
+# Hathaway, Michael J.
+# 20 October 2017
+
+# Document Title
+print ("Distance Traveled")
+
+# Define Inputs
+Speed = int(input("What is the Vehicle Speed in MPH? "))
+Hours = int(input("What is the Travel Time in Hours? "))
+
+
+# Loop function
+for i in range(1, Hours+1):
+    Distance = Speed * i
+    print (i, "     ", Distance)
+
+
+
+    
+        
